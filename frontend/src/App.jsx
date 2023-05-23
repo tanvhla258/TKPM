4e7d18efb5b3a342cdce93583463ffb6a62bab9f
tanvhla258 <tanvhla258@gmail.com>
2023-05-23 10:48:57 +0700

    product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,24 @@ import ProductsPage from "./pages/ProductsPage";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Header from "./components/Header";
 import OrdersPage from "./pages/OrdersPage";
+import SettingPage from "./pages/SettingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Sidebar />
-        <div className="content" style={{ marginLeft: "25 0px" }}>
-          <Header />
+        <Header />
+        <div
+          className="content"
+          style={{ marginTop: "100px", marginLeft: "250px", minHeight: "80vh" }}
+        >
+          <Sidebar />
           <div>
             <Routes>
               <Route exact path="/" element={<DashboardPage />} />
               <Route path="/products" element={<ProductsPage />} />
               <Route path="/orders" element={<OrdersPage />} />
+              <Route path="/settings" element={<SettingPage />} />
             </Routes>
           </div>
         </div>
