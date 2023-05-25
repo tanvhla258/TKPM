f5c28c8f5ed256d889b78909e4c8224b266db9ad
tanvhla258 <tanvhla258@gmail.com>
2023-05-25 19:49:24 +0700

    fix for deploy

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./DashboardPage.css";
-import PercentCircle from "../../components/percentCircle";
+import PercentCircle from "../../components/PercentCircle";
 import { Card, Typography, CardContent, Grid } from "@mui/material";
 const DashboardCard = () => (
   <Card sx={{ minWidth: 225 }}>
