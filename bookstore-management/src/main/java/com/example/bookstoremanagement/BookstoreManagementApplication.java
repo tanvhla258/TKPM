3eccbde30a8fe48c51e3f68ae575ad781d3481f9
cobemuadong <truong.pht.7080@gmail.com>
2023-06-01 23:47:20 +0700

    initial commit

---
+++ bookstore-management/src/main/java/com/example/bookstoremanagement/BookstoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagementApplication.class, args);
+	}
+
+}
